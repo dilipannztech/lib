32bf6f9f4a88066517a2a97dc238769f389b9c11
dilipannztech <dilip@annztech.com>
2024-05-15 15:16:04 +0530

    bcrypt function updated

--- paasword/bcryptFunctions.js
+++ paasword/bcryptFunctions.js
@@ -1,7 +1,9 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.checkPassword = exports.hashPassword = void 0;
 const bcrypt_1 = __importDefault(require("bcrypt"));
@@ -9,24 +11,22 @@ const bcrypt_1 = __importDefault(require("bcrypt"));
 const SALT_ROUNDS = 10;
 // Function to hash the password
 function hashPassword(password) {
-    try {
-        const salt = bcrypt_1.default.genSaltSync(SALT_ROUNDS);
-        const hashedPassword = bcrypt_1.default.hashSync(password, salt);
-        return hashedPassword;
-    }
-    catch (error) {
-        throw new Error(`Error hashing password: ${error}`);
-    }
+  try {
+    const salt = bcrypt_1.default.genSaltSync(SALT_ROUNDS);
+    const hashedPassword = bcrypt_1.default.hashSync(password, salt);
+    return hashedPassword;
+  } catch (error) {
+    throw new Error(`Error hashing password: ${error}`);
+  }
 }
 exports.hashPassword = hashPassword;
 // Function to check if the password matches the hashed password
 function checkPassword(password, hashedPassword) {
-    try {
-        const isMatch = bcrypt_1.default.compareSync(password, hashedPassword);
-        return isMatch;
-    }
-    catch (error) {
-        throw new Error(`Error checking password: ${error}`);
-    }
+  try {
+    const isMatch = bcrypt_1.default.compareSync(password, hashedPassword);
+    return isMatch;
+  } catch (error) {
+    throw new Error(`Error checking password: ${error}`);
+  }
 }
 exports.checkPassword = checkPassword;
